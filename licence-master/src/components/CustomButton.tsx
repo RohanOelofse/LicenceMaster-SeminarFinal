2f7e57efca294831a9b57708ad36606f1985eefe
RohanOelofse <rohanoelofse1@gmail.com>
2025-03-24 12:01:31 -0500

    Added way to change the type of button

--- licence-master/src/components/CustomButton.tsx
+++ licence-master/src/components/CustomButton.tsx
@@ -4,15 +4,24 @@ interface CustomButtonProps {
     width?: number
     height?: number
     title: string
+    color?: string
     onPress: () => void
 }
 
 function CustomButton(props: CustomButtonProps) {
-
-    return (
-        <button onClick={props.onPress} id="customButton">
-            <p style={{margin: 0, color: 'white', fontSize: 20, fontFamily:"Arial"}}>{props.title}</p>
-        </button>
-    )
+    if (props.color == undefined){
+        return (
+            <button onClick={props.onPress} id="customButton">
+                <p style={{margin: 0, color: 'white', fontSize: 20, fontFamily:"Arial"}}>{props.title}</p>
+            </button>
+        )
+    }else if (props.color == "blue"){
+        return (
+            <button onClick={props.onPress} id="customButtonBlue">
+                <p style={{margin: 0, color: 'white', fontSize: 20, fontFamily:"Arial"}}>{props.title}</p>
+            </button>
+        )
+    }
+    
 }
 export default CustomButton
