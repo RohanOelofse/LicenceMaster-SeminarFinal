de088466858d582f91704a618281cc4c629d9a12
RohanOelofse <rohanoelofse1@gmail.com>
2025-03-30 19:20:39 -0500

    Created TrafficSignals screen

---
+++ licence-master/src/screens/TrafficSignals.tsx
@@ -0,0 +1,25 @@
+import '../App.css'
+import "../styles/componentStyles.css"
+import CustomButton from "../components/CustomButton";
+import Footer from "../components/Footer";
+import HeaderBar from "../components/Headerbar";
+
+
+function TrafficSignals(){
+    return(
+        <>
+            <div id="appContainer">
+                <HeaderBar />
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px"}}>
+                    <h1>Main Menu</h1>
+                    <CustomButton title='Traffic Signals' onPress={() => {}}/>
+                    <CustomButton title='Traffic Signs' onPress={() => {}}/>
+                    <CustomButton title='Road Markings' onPress={() => {}}/>
+                    <CustomButton title='Rules of the Road' onPress={() => {}}/>
+                    <CustomButton color="blue" title='Practice Exam' onPress={() => {}}/>
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
+}
