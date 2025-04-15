0bdf89d4b46aa2df75d2127a17ed50f0ec182d2f
RohanOelofse <rohanoelofse1@gmail.com>
2025-04-15 17:40:38 -0500

    Added trafficSigns data

---
+++ licence-master/src/Data/trafficSigns.ts
@@ -0,0 +1,137 @@
+const trafficSignsData = [
+    {
+        id: 1,
+        image: '/TrafficSignals/stop.png',
+        information: `Stop - Come to a complete stop before entering an intersection. If there is
+            a painted stop line, stop at the line. Stop before entering a crosswalk,
+            painted or unpainted. If there is no crosswalk, stop at the safest point nearest the intersecting roadway to obtain a clear view of approaching traffic.`
+    },
+    {
+        id: 2,
+        image: '/TrafficSignals/yield.png',
+        information: `Yield - Slow down or stop, if necessary, to give the right-of-way to crossing or merging vehicles.`
+    },
+    {
+        id: 3,
+        image: '/TrafficSignals/speedLimit.png',
+        information: `Indicates the maximum speed that a vehicle can lawfully
+            travel on the roadway.`
+    },
+    {
+        id: 4,
+        image: '/TrafficSignals/noLeftTurn.png',
+        information: `No Left Turn - Left turn prohibited.`
+    },
+    {
+        id: 5,
+        image: '/TrafficSignals/noRightTurn.png',
+        information: `No Right Turn - Right turn prohibited.`
+    },
+    {
+        id: 6,
+        image: '/TrafficSignals/noUTurn.png',
+        information: `No U-Turn - U-turn prohibited.`
+    },
+    {
+        id: 7,
+        image: '/TrafficSignals/wrongWayDoNotEnter.png',
+        information: `Wrong Way & Do Not Enter - These signs are often used in combination, and identify a one-way street or expressway ramp. Do not drive
+            past either of these signs.`
+    },
+    {
+        id: 8,
+        image: '/TrafficSignals/oneWay.png',
+        information: `One Way - Traffic flows only in the direction of the arrow`
+    },
+    {
+        id: 9,
+        image: '/TrafficSignals/noParking.png',
+        information: `No Parking - Parking is prohibited.`
+    },
+    {
+        id: 10,
+        image: '/TrafficSignals/centerLaneLeftTurnOnly.png',
+        information: `Center Lane, Left Turn Only - A driver may use the lane only to make
+            a left turn.`
+    },
+    {
+        id: 11,
+        image: '/TrafficSignals/keepRight.png',
+        information: `Keep Right - A traffic island, median or obstruction is ahead. Keep to the
+            right.`
+    },
+    {
+        id: 12,
+        image: '/TrafficSignals/slowerTrafficKeepRight.png',
+        information: `Slower Traffic Keep Right - These signs are used on multiple-lane roadways to reduce unnecessary weaving and when there is a tendency on the
+            part of the motorist to drive in the left-hand lane (or lanes) below the normal speed of traffic.`
+    },
+    {
+        id: 13,
+        image: '/TrafficSignals/signalAhead.png',
+        information: `Signal Ahead - Traffic signal is ahead. Be prepared to stop.`
+    },
+    {
+        id: 14,
+        image: '/TrafficSignals/reduceSpeedAhead.png',
+        information: `Reduce Speed Ahead - Informs motorists of a reduced speed zone.`
+    },
+    {
+        id: 15,
+        image: '/TrafficSignals/stopAhead.png',
+        information: `Stop Ahead - Approaching a stop sign.`
+    },
+    {
+        id: 16,
+        image: '/TrafficSignals/yieldAhead.png',
+        information: `Yield Ahead - Approaching a yield sign.`
+    },
+    {
+        id: 17,
+        image: '/TrafficSignals/dividingHighwayBegins.png',
+        information: `Divided Highway Begins - The traffic lanes ahead are divided by a
+            median or divider and each lane is one-way. Keep to the right.`
+    },
+    {
+        id: 18,
+        image: '/TrafficSignals/dividingHighwayEnds.png',
+        information: `Divided Highway Ends - The oncoming traffic lane ahead is no longer
+            divided by median or divider. Keep to the right.`
+    },
+    {
+        id: 19,
+        image: '/TrafficSignals/twoWayTraffic.png',
+        information: `Yield Ahead - Approaching a yield sign.`
+    },
+    {
+        id: 20,
+        image: '/TrafficSignals/pedestrianCrossing.png',
+        information: `Pedestrian Crossing - Be alert for people crossing the street. Slow down,
+            yield right of way to pedestrians, or stop if necessary.`
+    },
+    {
+        id: 21,
+        image: '/TrafficSignals/bicycle.png',
+        information: `Bicycle - Be alert for bicyclists crossing the street.`
+    },
+    {
+        id: 22,
+        image: '/TrafficSignals/reverseTurn.png',
+        information: `Reverse Turn - Just ahead, the road turns sharply right, then left. May be
+            shown as left or right`
+    },
+    {
+        id: 23,
+        image: '/TrafficSignals/reverseCurve.png',
+        information: `Reverse Curve - Just ahead, the road curves right, then left. May be shown
+            as left or right.`
+    },
+    {
+        id: 24,
+        image: '/TrafficSignals/curve.png',
+        information: `Curve - There is a gradual curve to the right ahead. Slow down to the safe
+            speed indicated. May be shown as left or right`
+    },
+]
+
+export default trafficSignsData;
